e396dd7fcd9806b76a8b0ef55c92e9105b087ebd
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-02-14 00:13:43 +0100

    Update users.js

--- utils/users.js
+++ utils/users.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 const usersFile = "users.json";
 const secretKey = 'key_test';
 
-// Load users from JSON file
 function loadUsers() {
   if (fs.existsSync(usersFile)) {
     const data = fs.readFileSync(usersFile, 'utf8');
@@ -14,28 +13,21 @@ function loadUsers() {
   }
 }
 
-// Save users to JSON file
 function saveUsers(users) {
   const data = JSON.stringify(users, null, 2);
   fs.writeFileSync(usersFile, data, 'utf8');
 }
 
-// Function to find user by authentication token (JWT token)
 function findUserByAuthToken(token) {
   try {
-    // Decode the token to get user information
     const decoded = jwt.verify(token, secretKey);
     
-    // Load users from JSON file
     const users = loadUsers();
     
-    // Find user by username in the list of users
     const user = users.find(u => u.username === decoded.username);
     
-    // Return the user object if found
     return user;
   } catch (err) {
-    // Return null if token is invalid or user not found
     return null;
   }
 }
