be1ab92ebcd494bd1fcecdcbe836664b87673f17
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-02-13 23:13:21 +0100

    Update getUserInformation.js

--- routes/getUserInformation.js
+++ routes/getUserInformation.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const multer = require('multer');
+const { loadUsers } = require('../utils');
+
 const verifyToken = require('../utils/jwtAuthMiddleware');
+const upload = multer();
 
 
 const router = express.Router();
 
-router.get('/api/username', verifyToken, (req, res) => {
+router.get('/api/authuser', verifyToken, (req, res) => {
     const username = req.user.username;
 
     res.json({ username });
+});
+
+router.get('/api/uuiduser', upload.none(), (req, res) => {
+    const uuid = req.body.uuid;
+    const users = loadUsers();
+    const user = users.find(user => user.uuid === uuid); 
+
+    if (user) {
+        res.json({ username: user.username });
+    } else {
+        res.status(404).json({ error: "User not found" });
+    }
 });
 
 
