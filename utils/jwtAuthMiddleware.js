ce7d06103a48cc825919a514bb79a13cde16c6b6
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-02-14 22:44:14 +0100

    improved verifyToken
    The token is now only valid as long as the user still exists in the user database.
    With that improvement the user gets even logged out when the user gets deleted

--- utils/jwtAuthMiddleware.js
+++ utils/jwtAuthMiddleware.js
@@ -1,10 +1,14 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
+const { loadUsers } = require('../utils');
 
 const secretKey = 'test_key';
 
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
+  const users = loadUsers();
+
+  console.log(users);
 
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized: No token provided' });
@@ -13,9 +17,16 @@ const verifyToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, secretKey);
     req.user = decoded;
+
+    const userExists = users.some(u => u.username === decoded.username);
+    if (!userExists) {
+      return res.status(401).json({ message: 'Unauthorized: User not found' });
+    }
+
     next();
   } catch (err) {
     return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+
   }
 };
 
