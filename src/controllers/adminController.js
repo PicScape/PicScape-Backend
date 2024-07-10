a8908f90921da5caadceac402cdc54f94341a73a
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-07-10 14:12:37 +0200

    Update adminController.js

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,54 +1,45 @@
 const Account = require('../models/Account');
-const jwt = require('jsonwebtoken');
-const Pfp = require('../models/Pfp');
-const Wallpaper = require('../models/Wallpaper');
-const emailUtil = require('../utils/emailUtil');
-const crypto = require('crypto');
 
-const { JWT_SECRET } = process.env;
 
 
 
 
 const getAccounts = async (req, res) => {
-    const { sortField = 'name', sortOrder = 'asc', page = 1 } = req.query;
+    const { sortField = 'username', sortOrder = 'asc', page = 1 } = req.query;
     const limit = 40;
-    const sortOrderValue = sortOrder === 'asc' ? 1 : -1;
+    const sortOrderValue = sortOrder === 'dec' ? -1: 1;
 
     try {
         if (!Number.isInteger(+page) || +page <= 0) {
             return res.status(400).json({ error: 'Invalid page number specified' });
         }
 
-        if (!['name', 'creation_date'].includes(sortField)) {
+        if (!['username', 'created_at'].includes(sortField)) {
             return res.status(400).json({ error: 'Invalid sortField specified' });
         }
+
+        const totalAccounts = await Account.countDocuments();
 
         const results = await Account.find()
             .sort({ [sortField]: sortOrderValue })
             .skip((+page - 1) * limit)
             .limit(limit);
 
-        const formattedResults = await Promise.all(results.map(async (account) => {
-
-            
-
-            return {
-                id: account._id,
-                username: account.username,
-                email: account.email,
-                roles: account.roles,
-                isAdmin: account.isAdmin,
-                isActivated: account.isActivated,
-                created_at: account.created_at,
-                updated_at: account.updated_at,
-            };
+        const formattedResults = results.map(account => ({
+            id: account._id,
+            username: account.username,
+            email: account.email,
+            roles: account.roles,
+            isAdmin: account.isAdmin,
+            isActivated: account.isActivated,
+            created_at: account.created_at,
+            updated_at: account.updated_at,
         }));
 
         res.json({
             accounts: formattedResults,
             currentPage: +page,
-            totalPages: Math.ceil(results.length / limit)
+            totalPages: Math.ceil(totalAccounts / limit)
         });
     } catch (error) {
         console.error(`Error in getAccounts: ${error.message}`);
